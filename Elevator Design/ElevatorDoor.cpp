cafccd5e7171531daf3c0a455568638af5b22705
Bajikomara <kbaji52@gmail.com>
2024-10-06 19:27:58 +0530

    update1

--- Elevator Design/ElevatorDoor.cpp
+++
@@ -1,9 +0,0 @@
-#include "ElevatorDoor.hpp"
-#include<bits/stdc++.h>
-using namespace std;
-void ElevatorDoor :: openDoor(){
-    std::cout << "Opening the door" << endl; 
-}
-void ElevatorDoor :: closeDoor(){
-    std:: cout << "Closing the door" << endl;
-}
